d232ae73d6e786909c9b429687565b84c0a8f085
Andrew Black <elystus@gmail.com>
2015-07-09 16:58:02 -0700

    Responsive Header

--- assets/js/wolfe.js
+++ assets/js/wolfe.js
@@ -1,6 +1,35 @@
-$(document).ready(
-       
-       $(window).scroll( function() {
-           
-       })
-);
+$(document).ready( function() {
+    
+    // Menu Fixed Handler
+    var  pageheadHeight  = $('#blog-masthead').height(),
+         pageMenu        = $('#blog-menu');
+
+    menuAlign();
+
+    $(window).scroll( function() {
+        menuAlign();
+    });
+
+    function menuAlign() {
+        var windowTop = $(window).scrollTop();
+
+        if(windowTop > pageheadHeight) {
+            pageMenu.removeClass('brand-hidden');
+            pageMenu.removeClass('blog-menu-inline');
+        } else if (windowTop < pageheadHeight) {
+            pageMenu.addClass('brand-hidden');
+            pageMenu.addClass('blog-menu-inline');
+        }
+    }
+
+    // Dropdown Menu Handler
+    var  dropdownToggle  = $('#dropdown-toggle'),
+         dropdownMenu    = $('#dropdown-menu');
+    
+    dropdownToggle.click(function() {
+        if(dropdownMenu.is(":visible"))
+            dropdownMenu.slideUp("slow");
+        else
+            dropdownMenu.slideDown("slow");
+    });        
+});
