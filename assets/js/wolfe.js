0dc67802713e7cac98864b1bacee7b90f61e058b
Andrew Black <elystus@gmail.com>
2016-03-31 19:10:22 -0700

    Wolfe alpha version 2

--- assets/js/wolfe.js
+++ assets/js/wolfe.js
@@ -1,35 +1,14 @@
-$(document).ready( function() {
-    
-    // Menu Fixed Handler
-    var  pageheadHeight  = $('#blog-masthead').height(),
-         pageMenu        = $('#blog-menu');
-
-    menuAlign();
-
-    $(window).scroll( function() {
-        menuAlign();
-    });
-
-    function menuAlign() {
-        var windowTop = $(window).scrollTop();
-
-        if(windowTop > pageheadHeight) {
-            pageMenu.removeClass('brand-hidden');
-            pageMenu.removeClass('blog-menu-inline');
-        } else if (windowTop < pageheadHeight) {
-            pageMenu.addClass('brand-hidden');
-            pageMenu.addClass('blog-menu-inline');
+$(document).ready(function() {
+    window.onscroll = function() {
+        var scrollTop     = $(window).scrollTop(),
+            elementOffset = $('#navbar-offset').offset().top;
+        
+        var navbarElement = $('#navbar');
+        
+        if ( scrollTop >= elementOffset ) {
+            navbarElement.addClass("navbar-fixed-top");
+        } else {
+            navbarElement.removeClass("navbar-fixed-top");
         }
-    }
-
-    // Dropdown Menu Handler
-    var  dropdownToggle  = $('#dropdown-toggle'),
-         dropdownMenu    = $('#dropdown-menu');
-    
-    dropdownToggle.click(function() {
-        if(dropdownMenu.is(":visible"))
-            dropdownMenu.slideUp("slow");
-        else
-            dropdownMenu.slideDown("slow");
-    });        
+    };
 });
